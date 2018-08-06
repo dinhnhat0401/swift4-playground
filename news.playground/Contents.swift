581a9ce8dc5a5aa355d862284d7a929ecfb7d442
dinhnhat0401 <nhatphutho@gmail.com>
2018-08-06 23:24:36 +0900

    Add first playground file

---
+++ news.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground 😀"
+
+//str.forEach {
+//    print($0)
+//}
+
+//for char in str {
+//    print(char);
+//}
+
+print(str.isEmpty)
+//print(str.dropFirst())
+print(String(str.reversed()))
+str.count
+print(str.last!.unicodeScalars[0].isASCII)
+let str1 = str.filter { c in
+    let isASCII = c.unicodeScalars.reduce(true, { x, y in
+        y.isASCII
+    })
+    return isASCII
+}
+print(str1)
+
+let numbers = [1, 2, 3, 4]
+let numberSum = numbers.reduce(0, { x, y in
+    x + y
+})
+print(numberSum)
+
+let text = """
+First row,
+Second row,
+Third row
+"""
+
+extension String {
+    func useMapPattern(_ i: Int) -> String {
+        return map { String($0)}[i]
+    }
+    
+    func useIndexPattern(_ i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
